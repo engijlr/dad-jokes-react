7d84b7befae43512b056f3fa14134a174e25ac11
Engi Lozada <engilozada@gmail.com>
2023-02-01 00:40:38 +0100

    Fix Joke component & Add styles

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Joke from "./Joke.js";
 import axios from "axios";
+import "./JokeList.css";
+import { v4 as uuidv4 } from "uuid";
 
 class JokeList extends Component {
   static defaultProps = {
@@ -9,6 +11,7 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = { jokes: [] };
+    //this.handleVote = this.handleVote.bind(this);
   }
   async componentDidMount() {
     let jokes = [];
@@ -16,17 +19,37 @@ class JokeList extends Component {
       let res = await axios.get("https://icanhazdadjoke.com/", {
         headers: { Accept: "application/json" },
       });
-      jokes.push(res.data.joke);
+      jokes.push({ text: res.data.joke, votes: 0, id: uuidv4() });
     }
     this.setState({ jokes: jokes });
+  }
+  handleVote(id, delta) {
+    this.setState((st) => ({
+      jokes: st.jokes.map((j) =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      ),
+    }));
   }
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map((j) => (
-            <Joke joke={j} />
+            <Joke
+              text={j.text}
+              votes={j.votes}
+              key={j.id}
+              id={j.id}
+              upvote={() => this.handleVote(j.id, 1)}
+              downvote={() => this.handleVote(j.id, -1)}
+            />
           ))}
         </div>
       </div>
